928d585f213e7727dd2d73af13647ecf9e30816a
kaprielovag <kaprielovag@gmail.com>
2017-08-22 22:10:00 +0300

    'some-commit'

--- js/pictures.js
+++ js/pictures.js
@@ -4,19 +4,28 @@ var pictures = [];
 var comments = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 var likesMax = 256;
 var likesMin = 15;
+var fragment = document.createDocumentFragment();
 var pictureContainer = document.querySelector('.pictures');
 var pictureTemplate = document.querySelector('#picture-template').content.querySelector('.picture');
 var pictureTemplateImg = pictureTemplate.querySelector('img');
-var pictureTemplateComments = pictureTemplate.querySelector('.picture-comments');
 var pictureTemplateLikes = pictureTemplate.querySelector('.picture-likes');
+var pictureTemplateComments = pictureTemplate.querySelector('.picture-comments');
+var uploadOverlay = document.querySelector('.upload-overlay');
+var galleryOverlay = document.querySelector('.gallery-overlay');
 
 function getRandomArbitrary(min, max) {
   return Math.round(Math.random() * (max - min) + min);
 }
 
-for (var i = 1; i <= 25; i++) {
+for (var i = 0; i <= 25; i++) {
   pictures[i] = {'url': 'photos/' + (i + 1) + '.jpg', 'likes': getRandomArbitrary(likesMin, likesMax), 'comment': comments[getRandomArbitrary(0, comments.length - 1)]};
   var pictureTemplateClone = pictureTemplate.cloneNode(true);
   pictureTemplateImg.setAttribute('src', pictures[i].url);
-  pictureContainer.appendChild(pictureTemplateClone);
+  pictureTemplateLikes.insertAdjacentText('afterbegin', pictures[i].likes);
+  pictureTemplateComments.insertAdjacentText('afterbegin', pictures[i].comment);
+  fragment.appendChild(pictureTemplateClone);
 }
+pictureContainer.appendChild(fragment);
+// uploadOverlay.classList.add('hidden');
+//
+// galleryOverlay.classList.remove('hidden');
